712f36416942d557a7a3614434760fe1b97f5904
Chris Kipp <ckipp@pm.me>
2022-04-02 10:37:25 +0200

    refactor: make all paths relative

--- run.sh
+++ run.sh
@@ -11,6 +11,7 @@ ERROR_MESSAGE_ID="$2"
 BLUE='\033[00;34m'
 RED='\033[00;31m'
 END='\033[0m'
+SOURCE_ROOT=$PWD
 
 function error() {
   echo -e "$RED $1$END"
@@ -29,27 +30,43 @@ if [ ! -d ./out ]; then
   mkdir -p ./out
 fi
 
-if [ $SCALA_VERSION ]; then
-  info "Running with version $SCALA_VERSION"
-else
-  error "Unable to get Scala nightly"
-fi
+function check_scala_version() {
+  if [ $SCALA_VERSION ]; then
+    info "Running with version $SCALA_VERSION"
+  else
+    error "Unable to get Scala nightly"
+    exit 1
+  fi
+}
+
+# So this is a bit of a hack because we don't want the full path in here or the check will fail for CI
+# So this command just changes:
+# -- [E001] Syntax Error: /Users/ckipp/Documents/scala-workspace/dotty-error-index/./0001_EmptyCatchBlockID.scala:2:9
+# with
+# -- [E001] Syntax Error: ./0001_EmptyCatchBlockID.scala:2:9
+# NOTE: we also use ? instea of / since the PWD has / in it and screws up sed because computers are dumb
+function relativize () {
+  sed -i "" "s?$SOURCE_ROOT/??g" $1
+}
 
 function update_file() {
   NEW_NAME=${1%.scala}.check
   cs launch scala:$SCALA_VERSION -- -color:never -explain -deprecation -source:future -Ycook-docs $1 &> checkfiles/$NEW_NAME
-  cat checkfiles/$NEW_NAME
+  relativize checkfiles/$NEW_NAME
+  info "Updated checkfiles/$NEW_NAME"
 }
 
 function handle_update() {
   if [ $ERROR_MESSAGE_ID ]; then
     TARGET_FILE=$(find . -maxdepth 1 -type f -name $ERROR_MESSAGE_ID*)
     if [ $TARGET_FILE ]; then
+      check_scala_version
       update_file $TARGET_FILE
     else
       error "Found no file starting with $ERROR_MESSAGE_ID"
     fi
   else
+    check_scala_version
     for TARGET_FILE in $TARGET_FILES
     do
       update_file $TARGET_FILE
@@ -60,6 +77,7 @@ function handle_update() {
 function check_file() {
   NEW_NAME=${1%.scala}.check
   cs launch scala:$SCALA_VERSION -- -color:never -explain -deprecation -source:future -Ycook-docs $1&> out/$NEW_NAME
+  relativize out/$NEW_NAME
   diff out/$NEW_NAME checkfiles/$NEW_NAME && \
     info "$NEW_NAME matches expected output" || \
     (error "$NEW_NAME doesn't match expected output" && exit 1)
@@ -69,11 +87,13 @@ function handle_check() {
   if [ $ERROR_MESSAGE_ID ]; then
     TARGET_FILE=$(find . -maxdepth 1 -type f -name $ERROR_MESSAGE_ID*)
     if [ $TARGET_FILE ]; then
+      check_scala_version
       check_file $TARGET_FILE
     else
       error "Found no file starting with $ERROR_MESSAGE_ID"
     fi
   else
+    check_scala_version
     for TARGET_FILE in $TARGET_FILES
     do
       check_file $TARGET_FILE || exit 1
@@ -81,18 +101,24 @@ function handle_check() {
   fi
 }
 
+function run_file() {
+  cs launch scala:$SCALA_VERSION -- -explain -deprecation -source:future -Ycook-docs $1
+}
+
 function handle_run() {
   if [ $ERROR_MESSAGE_ID ]; then
     TARGET_FILE=$(find . -maxdepth 1 -type f -name $ERROR_MESSAGE_ID*)
     if [ $TARGET_FILE ]; then
-      cs launch scala:$SCALA_VERSION -- -explain -deprecation -source:future -Ycook-docs $TARGET_FILE
+      check_scala_version
+      run_file $TARGET_FILE
     else
       error "Found no file starting with $ERROR_MESSAGE_ID"
     fi
   else
+    check_scala_version
     for TARGET_FILE in $TARGET_FILES
     do
-      cs launch scala:$SCALA_VERSION -- -explain -deprecation -source:future -Ycook-docs $TARGET_FILE
+      run_file $TARGET_FILE
     done
   fi
 }
