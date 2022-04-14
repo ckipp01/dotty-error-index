49c6749c8d940787da8781241a5a2d7a13d6356f
Rikito Taniguchi <rikiriki1238@gmail.com>
2022-04-14 17:48:57 +0900

    refactor: simplify the example for 061

--- examples/061_TopLevelCantBeImplicitID.scala
+++ examples/061_TopLevelCantBeImplicitID.scala
@@ -1,9 +1,5 @@
 // LEGACY In the past top level implicit were not allowed. This is however no longer the case as of https://github.com/lampepfl/dotty/pull/5754
 // START
 @main def TopLevelCantBeImplicitID = ()
-
-import scala.language.implicitConversions
-case class C(str: String)
-implicit def toC(x: String): C = C(x)
-implicit val defaultC: C = C("default")
+implicit val foo: String = "foo"
 // END
