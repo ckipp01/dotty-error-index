238a93ac9cf3481df1d6bd4e786378f3bf4a480c
Rikito Taniguchi <rikiriki1238@gmail.com>
2022-04-14 16:35:49 +0900

    fead: add in 061
    Adding `// LEGACY` to 061.
    It seems like TopLevelCantBeImplicit is no longer the case thanks to
    https://github.com/lampepfl/dotty/pull/5754
    
    This is actually confirmed in https://github.com/lampepfl/dotty/blob/93fc41fcb624df73cc12d52b79d518a30a778a7c/tests/run/toplevel-implicits/a.b.scala#L19-L21

--- examples/061_TopLevelCantBeImplicitID.scala
+++ examples/061_TopLevelCantBeImplicitID.scala
@@ -1,6 +1,9 @@
-// I can't seem to reproduce this one. I see in `Checking.scala` that is should
-// be it if sym.owner.is(Package), but when playing around that doesn't seem to
-// ever be true
-// INCOMPLETE
+// LEGACY In the past top level implicit were not allowed. This is however no longer the case as of https://github.com/lampepfl/dotty/pull/5754
+// START
 @main def TopLevelCantBeImplicitID = ()
 
+import scala.language.implicitConversions
+case class C(str: String)
+implicit def toC(x: String): C = C(x)
+implicit val defaultC: C = C("default")
+// END
